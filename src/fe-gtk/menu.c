f287ee97d840e76dd5dae8a39d7023a278048c27
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2026-01-06 01:21:58 -0700

    Update URLs in menu.c for documentation and website

--- src/fe-gtk/menu.c
+++ src/fe-gtk/menu.c
@@ -1613,7 +1613,7 @@ menu_ctcpguiopen (void)
 static void
 menu_docs (GtkWidget *wid, gpointer none)
 {
-	fe_open_url ("http://zoitechat.readthedocs.org");
+	fe_open_url ("http://zoitechat.zoite.net/docs");
 }
 
 /*static void
@@ -1758,7 +1758,7 @@ menu_about (GtkWidget *wid, gpointer sess)
 	gtk_about_dialog_set_program_name (dialog, _(DISPLAY_NAME));
 	gtk_about_dialog_set_version (dialog, PACKAGE_VERSION);
 	gtk_about_dialog_set_license (dialog, license); /* gtk3 can use GTK_LICENSE_GPL_2_0 */
-	gtk_about_dialog_set_website (dialog, "http://zoitechat.github.io");
+	gtk_about_dialog_set_website (dialog, "http://zoitechat.zoite.net");
 	gtk_about_dialog_set_website_label (dialog, "Website");
 	gtk_about_dialog_set_logo (dialog, pix_zoitechat);
 	gtk_about_dialog_set_copyright (dialog, "\302\251 1998-2010 Peter \305\275elezn\303\275\n\302\251 2009-2014 Berke Viktor");
